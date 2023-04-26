d57cee845fafc57f26f69fb980a2b48499347da4
Arthur Ritzel <arthurritzel@users.noreply.github.com>
2023-04-26 01:12:33 -0300

    aula

---
+++ aula002/aula002.js
@@ -0,0 +1,2 @@
+var nome = window.prompt("Qual seu nome:")
+alert('É um prazer te conhecer ' + nome + '!')
---
+++ aula002/aula002b.js
@@ -0,0 +1,4 @@
+var n1 = Number.parseFloat(window.prompt('Digite um numero'))
+var n2 = Number.parseFloat(window.prompt('Digite um numero'))
+var s = n1 + n2
+alert(s)
