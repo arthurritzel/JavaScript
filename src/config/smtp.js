7235e3f373f8c37fdee9577f84d841f4eb14f9d3
Arthur Ritzel <arthurritzel@users.noreply.github.com>
2023-05-24 15:44:31 -0300

    teste

---
+++ src/config/smtp.js
@@ -0,0 +1,6 @@
+module.exports = {
+    host: "smpt.gmail.com",
+    port: 587,
+    user: "mapainterativoon3@gmail.com",
+    pass: "On3imobiliaria2023@"
+}
---
+++ src/index.js
@@ -0,0 +1,7 @@
+const nodemailer = require('nodemailer');
+
+const SMPT_CONFIG = REQUIRE('./config/smpt');
+
+const transporter = nodemailer.createTransport({
+    host: 
+})
