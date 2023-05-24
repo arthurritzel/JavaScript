d7ee961a2134b77b37425817529b4967cd1e4d92
Arthur Ritzel <arthurritzel@users.noreply.github.com>
2023-05-24 13:14:15 -0300

    cursojs

---
+++ curso js/script.js
@@ -0,0 +1,6 @@
+function funcao(nome, sobrenome, idade = 18){
+    
+    console.log(nome, sobrenome, idade);
+}
+
+funcao("Arthur", "Ritzel");
---
+++ curso js/script2.js
@@ -0,0 +1,5 @@
+console.log(name);
+
+function nome (){
+    var name = 'Arthur';
+}
